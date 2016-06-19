8b678bba2ecb0cbf9707c03caf17ad13fe7e43d5
Dirken <ricard.meyerhofer@est.fib.upc.edu>
2016-06-19 22:17:46 +0200

    finished first part

--- python/practica.py
+++ python/practica.py
@@ -11,6 +11,7 @@ class restaurant:
 
     def afegir_nom(self,nom):       self.nom = nom
     def afegir_adr(self,adr):       self.adr = adr
+    def afegir_distr(self,distr):   self.distr = distr
     def afegir_barri(self,barri):   self.barri = barri
     def afegir_ciutat(self,ciutat): self.ciutat = ciutat
     def afegir_reg(self,reg):       self.reg = reg
@@ -21,8 +22,7 @@ class restaurant:
     def afegir_mail(self,mail):     self.mail = mail
     def afegir_codi(self,codi):     self.codi = codi
     def afegir_tel(self,tel):
-        if hasattr(self,'telefon'):
-            self.tel.append(tel)
+        if hasattr(self,'telefon'): self.tel.append(tel)
         else:
             self.tel = []
             self.tel.append(tel)
@@ -40,7 +40,6 @@ class MHTMLParser(HTMLParser):
         elif tag == 'v:url':
             for nom, val in attrs:
                 if nom == 'rdf:resource':       self.crest.afegir_web(val)
-
         elif tag == 'rdf:description':
             for nom, val in attrs:
                 if nom == 'rdf:about':          self.crest.afegir_mail(val)
@@ -53,6 +52,7 @@ class MHTMLParser(HTMLParser):
         if self.ctag == 'v:fn':                 self.crest.afegir_nom(data)
         elif self.ctag == 'v:locality':         self.crest.afegir_ciutat(data)
         elif self.ctag == 'xv:neighborhood':    self.crest.afegir_barri(data)
+        elif self.ctag == 'xv:district':        self.crest.afegir_distr(data)
         elif self.ctag == 'v:latitude':         self.crest.afegir_lat(data)
         elif self.ctag == 'v:postal-code':      self.crest.afegir_codi(data)
         elif self.ctag == 'v:longitude':        self.crest.afegir_lon(data)
@@ -70,11 +70,11 @@ parser = MHTMLParser()
 parser.feed(rdfSource)
 print len(allrest)
 
-restaurantfile = open('restaurants.csv','wb')
-restaurantwriter = csv.writer(restaurantfile)
+data = open('restaurants.csv','wb')
+data2 = csv.writer(data)
 
-head = ["nom"]+["telefons"]+["direccio"]+["barri"]+["ciutat"]+["codi postal"]+["regio"]+["pais"]+["latitud"]+["longitud"]+["web"]+["mail"]
-restaurantwriter.writerows([head])
+head = ["Nom"]+["Telefon"]+["Direccio"]+["Districte"]+["Barri"]+["Ciutat"]+["Codi Postal"]+["Regio"]+["Pais"]+["Lat"]+["Lon"]+["Web"]+["Mail"]
+data2.writerows([head])
 
 for r in allrest:
     nom = [r.nom]
@@ -89,6 +89,7 @@ for r in allrest:
                 telf = telf+','+it
 
     else: telf = '-'
+
     if hasattr(r,'adress'): adress = [r.adr]
     else:                   adress = ['-']
 
@@ -103,13 +104,17 @@ for r in allrest:
     if hasattr(r,'mail'):   mail = [r.mail]
     else:                   mail = ['-']
 
+    if hasattr(r,'distr'):  distr = [r.distr]
+    else:                   distr = ['-']
+
     ciutat = [r.ciutat]
     regio = [r.reg]
     pais = [r.pais]
     lat = [r.lat]
     lon = [r.lon]
 
-    output = nom + [telf] + adress + barri + ciutat + codi + regio + pais + lat + lon + url + mail
-    restaurantwriter.writerows([output])
-restaurantfile.close()
+    output = nom + [telf] + adress + distr + barri + ciutat + codi + regio + pais + lat + lon + url + mail
+    data2.writerows([output])
+
+data.close()
  
