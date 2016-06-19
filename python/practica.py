34b37ce61ccb4614b1609c03a21043c2141ed7bc
Dirken <ricard.meyerhofer@est.fib.upc.edu>
2016-06-19 15:25:23 +0200

    omw2

--- python/practica.py
+++ python/practica.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+# -*- coding: utf-8 -*-
 import sys
 
 from HTMLParser import HTMLParser
@@ -10,6 +10,17 @@ class restaurant:
 
     def afegir_nom(self,nom):
         self.nom = nom
+    def afegir_sect(self,nom):
+        self.sect = nom
+
+    #def afegir_adre(self,nom):
+     #   self.nom = nom
+    #def afegir_lat(self,):
+    #def afegir_lon():
+    #def afegir_telf():
+    #def afegir_mail():
+    #def afegir_web():
+    #def afegir_barri():
 
         
 # creem una subclasse i sobreescribim el metodes del han
@@ -31,6 +42,19 @@ class MHTMLParser(HTMLParser):
     def handle_data(self, data):
         if self.ctag == 'v:fn':
             self.crest.afegir_nom(data)
+        elif self.ctag == 'v:section':
+            self.crest.afegir_sect(data)
+
+        #elif self.ctag == 'v:adapted':
+        #elif self.ctag == 'v:adr':
+        #elif self.ctag == 'v:geo':
+        #elif self.ctag == 'v:tel':
+        #elif self.ctag == 'xv:schedule':
+        #elif self.ctag == 'dct:isPartOf':
+        #elif self.ctag == 'v:category':
+        #elif self.ctag == 'dct:created':
+        #elif self.ctag == 'v:rev':'''
+
 
 f = open('restaurants.rdf', 'rb') # obre l'arxiu
 rdfSource = f.read()                            
@@ -41,4 +65,5 @@ parser.feed(rdfSource)
 print len(allrest)
 for r in allrest:
     print r.nom
+    print r.sect
 
